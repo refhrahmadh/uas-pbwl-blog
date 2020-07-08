8f4b4430fe468555048f489eae8e26c193168b73
refhrahmadh <refhrahmadh.7@gmail.com>
2020-07-08 17:26:26 +0700

    uas ganjil

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" style="color: #000;">DASHBOARD</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <img src="image/das.jpg" class="img">
+                    <br><br>
+                    <div style="color: #000;">Anda Telah Masuk 
+                    <br>
+                    SILAHKAN BERBELANJA SEPUASNYA ! </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
